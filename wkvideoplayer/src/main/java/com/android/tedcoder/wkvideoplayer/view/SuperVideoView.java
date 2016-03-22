6ac9e96742e8ba833d9a67883464ce1eca952248
aack <389620129@qq.com>
2016-03-22 15:05:06 +0800

    加入视频播放,视频来源优酷和迅雷

---
+++ wkvideoplayer/src/main/java/com/android/tedcoder/wkvideoplayer/view/SuperVideoView.java
@@ -0,0 +1,31 @@
+package com.android.tedcoder.wkvideoplayer.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.VideoView;
+
+/**
+ * Created by Ted on 2015/8/6.
+ */
+public class SuperVideoView extends VideoView {
+
+    public SuperVideoView(Context context) {
+        super(context);
+        init();
+    }
+
+    public SuperVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public SuperVideoView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public void init(){
+        setKeepScreenOn(true);
+    }
+
+}
